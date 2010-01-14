6c67721b8f4e6173af181edeef24a7baff26bcfb
Ali Farhadi <a.farhadi@gmail.com>
2010-01-14 19:04:02 +0330

    Adding some more tests

--- tests/tests.php
+++ tests/tests.php
@@ -42,6 +42,80 @@ class IntlDateTimeTestCase extends UnitTestCase {
 		$this->assertEqual($result, '2010/01/13 12:42:20');
 	}
 
+	function testSet(){
+		$date = new IntlDateTime('now', 'Asia/Tehran', 'gregorian');
+
+		$date->set('2009/1/2 01:00 PM');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2009/01/02 13:00:00');
+
+		$date->set('2009-10-25');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2009/10/25 00:00:00');
+
+		$date->set('09/05/02 14:00');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2009/05/02 14:00:00');
+
+		$date->set('14 Jan 2010');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2010/01/14 00:00:00');
+
+		$date->set('Wed, 7 Jan 09');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2009/01/07 00:00:00');
+
+		$date->set('Monday, 15 March 2010 06:22:30 PM');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2010/03/15 18:22:30');
+
+		$date->set(strtotime('25 Dec 2009'));
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2009/12/25 00:00:00');
+
+		$date->set(new DateTime('15 Jan 2010'));
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2010/01/15 00:00:00');
+
+		$date->set('now');
+		$result = $date->getTimestamp();
+		$expected = strtotime('now');
+		$this->assertEqual($result, $expected);
+
+		$date->set('yesterday');
+		$result = $date->getTimestamp();
+		$expected = strtotime('yesterday');
+		$this->assertEqual($result, $expected);
+
+		$date->set('2010/01/10', 'UTC');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2010/01/10 03:30:00');
+
+		$date->set('2010/07/10', 'UTC');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2010/07/10 04:30:00');
+
+		$date->set('20100412182457', null, 'yyyyMMddHHmmss');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '2010/04/12 18:24:57');
+
+		$date->setLocale('fa');
+
+		$date->set('دوشنبه ۱۵ مارس ۲۰۱۰ ۱۲:۳۲:۴۵');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '۲۰۱۰/۰۳/۱۵ ۱۲:۳۲:۴۵');
+
+		$date->setCalendar('persian');
+
+		$date->set('۲۳ دی ۱۳۸۸');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '۱۳۸۸/۱۰/۲۳ ۰۰:۰۰:۰۰');
+
+		$date->set('۱۳۸۸-۱۱-۲۲ ۲۳:۵۰');
+		$result = $date->format('yyyy/MM/dd HH:mm:ss');
+		$this->assertEqual($result, '۱۳۸۸/۱۱/۲۲ ۲۳:۵۰:۰۰');
+	}
+
 	function testGetTimestamp(){
 		$date = new IntlDateTime('2010/01/01');
 		$result = $date->getTimestamp();
